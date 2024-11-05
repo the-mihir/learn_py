1367cfaa614f7f78cae32b052ed92d6148d6929f
Mihir <mihirkantho@gmail.com>
2024-11-06 01:43:04 +0600

    just learning Variable in paython

--- hello_py/app.py
+++ hello_py/app.py
@@ -1,10 +1,24 @@
-print('Hello python!')
-print('This is Mihir!')
+# variable in python 
 
-print('*' * 10)
-#======= This is for commentSingle line comment ===========
+price = 89 # this is a variable for price
+price = 90 # this is a variable for price and its updated integer number
+rating = 4.9 # this is a variable for rating and it is a float number
+is_published = True # this is a variable for boolean (Boolean should withe True or False)
+name = " Mihir Das" # this is a variable for string
+
+
+patient_name = "John Smith"
+patient_age = 20
+patient_is_new = True
+
+# Geting input from user
 '''
-This is a multiline "comment" in Python.
-It can span multiple lines and does not need any hash symbols.
+name = input('What is your name? ') # this is a function for getting input from user
+get_color = input('What is your favorite color? ')
+print(name +' your favorite color is '+ get_color)
 '''
-print("This is after multiline commnets")
+
+birth_year= input('Your Birth Year? ')
+age  = 2024 - int(birth_year)
+
+print(age)
