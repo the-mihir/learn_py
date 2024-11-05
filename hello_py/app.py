f2943c043c83812ebdbffc7f7843c9c79b8ad31f
Mihir <mihirkantho@gmail.com>
2024-11-06 01:14:07 +0600

    Initial comments

---
+++ hello_py/app.py
@@ -0,0 +1,10 @@
+print('Hello python!')
+print('This is Mihir!')
+
+print('*' * 10)
+#======= This is for commentSingle line comment ===========
+'''
+This is a multiline "comment" in Python.
+It can span multiple lines and does not need any hash symbols.
+'''
+print("This is after multiline commnets")
